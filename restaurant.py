d4bcb0c5d803029d7038fda9ea1aa084a3c828e2
mattyocode <matthewoliver@live.co.uk>
2020-08-31 22:09:47 +0100

    JSON menu file set up. Returning first menu item as dict

--- restaurant.py
+++ restaurant.py
@@ -1,9 +1,28 @@
 import config
+import json
 
 class Menu:
 
-    def __init__(self, items={}):
-        self.items = items
+    def __init__(self, name):
+        self.__name = name
+        self.__items= {}
 
     def get_menu(self):
-        return self.items
+        return self.__items
+
+    def set_menu(self, file):
+        with open(file) as json_file:
+            self.__items = json.load(json_file)
+        return self.__items
+
+    def print_menu(self):
+        if self.__items != '':
+            return self.__items["reg-cod"]
+        else:
+            return "No menu set"
+
+    def neat_print(self):
+        
+
+    def print_id(self):
+        pass
--- tests/test_restaurant.py
+++ tests/test_restaurant.py
@@ -1,7 +1,18 @@
 import pytest
 from restaurant import *
 
-subject = Menu()
+f = 'menu_items.json'
+subject = Menu(name="Chips")
 
 def test_menu_returns_dictionary():
     assert subject.get_menu() == {}
+
+# def test_get_item_id_num():
+#     assert subject.
+
+def test_get_first_menu_item():
+    subject.set_menu(f)
+    assert subject.print_menu() == {'id': '001', 'description': 'regular cod', 'price': 7}
+
+# def test_neat_print():
+#     subject.set_menu(f)    
