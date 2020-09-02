c292e525fa0fb2de2e89fca3f6bccb18ff6196f8
mattyocode <matthewoliver@live.co.uk>
2020-09-02 22:36:22 +0100

    Added test for non yes/no answer

--- display.py
+++ display.py
@@ -2,10 +2,12 @@ import restaurant
 
 def greeting():
     answer = input("Hello, would you like to see a menu? y/n")
-
-    if answer == "n":
-        return "No problem!"
-    elif answer == "y":
-        return "Great! Here's our menu: "
-    else:
-        return "Please try again!"
+    while True:
+        if answer == "n":
+            return "No problem!"
+            break
+        elif answer == "y":
+            return "Great! Here's our menu: "
+            break
+        else:
+            return "Please try again!"
--- tests/test_display.py
+++ tests/test_display.py
@@ -12,4 +12,6 @@ def test_yes_returns_menu(monkeypatch):
     with mock.patch.object(builtins, 'input', lambda _: 'y'):
         assert display.greeting() == "Great! Here's our menu: "
 
-
+def test_neither_returns_try_again(monkeypatch):
+    with mock.patch.object(builtins, 'input', lambda _: 'blah'):
+        assert display.greeting() == "Please try again!"
