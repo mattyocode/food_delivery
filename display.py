a06cd15da47ff3b037a0c23785afd808fcd8f318
mattyocode <matthewoliver@live.co.uk>
2020-09-03 20:57:54 +0100

    Refactored greeting method

--- display.py
+++ display.py
@@ -1,13 +1,22 @@
 import restaurant
 
-def greeting():
-    answer = input("Hello, would you like to see a menu? y/n")
-    while True:
-        if answer == "n":
-            return "No problem!"
-            break
-        elif answer == "y":
-            return "Great! Here's our menu: "
-            break
-        else:
-            return "Please try again!"
+class Display:
+
+    def __init__(self):
+        pass
+
+    def greeting(self):
+
+        while True:
+            answer = input("Hello, would you like to see a menu? y/n:> ")
+            if answer not in ('y', 'n'):
+                print("Didn't understand. Please try again!")
+                continue
+            else:
+                if answer == "n":
+                    return "No problem!"
+                elif answer == "y":
+                    return "Great! Here's our menu: "
+
+    # def show_menu(self, restaurant):
+        
--- tests/test_display.py
+++ tests/test_display.py
@@ -4,14 +4,16 @@ import builtins
 
 import display
 
+subject = display.Display()
+
 def test_no_returns_message(monkeypatch):
     with mock.patch.object(builtins, 'input', lambda _: 'n'):
-        assert display.greeting() == "No problem!"
+        assert subject.greeting() == "No problem!"
 
 def test_yes_returns_menu(monkeypatch):
     with mock.patch.object(builtins, 'input', lambda _: 'y'):
-        assert display.greeting() == "Great! Here's our menu: "
+        assert subject.greeting() == "Great! Here's our menu: "
 
-def test_neither_returns_try_again(monkeypatch):
-    with mock.patch.object(builtins, 'input', lambda _: 'blah'):
-        assert display.greeting() == "Please try again!"
+# def test_neither_returns_try_again(monkeypatch):
+#     with mock.patch.object(builtins, 'input', lambda _: 'blah'):
+#         assert display.greeting() == "Hello, would you like to see a menu? y/n:> "
