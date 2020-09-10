d6ecf910ffbd993e93887f8ed0eb8cc94e32c1db
mattyocode <matthewoliver@live.co.uk>
2020-09-10 18:40:58 +0100

    Menu showing in CLI, tests passing

--- tests/test_display.py
+++ tests/test_display.py
@@ -11,30 +11,34 @@ subject = Display()
 r = Restaurant("Chipps")
 stub_menu = Mock(Menu(r))
 
-# def test_no_returns_message(monkeypatch):
-#     with mock.patch.object(builtins, 'input', lambda _: 'n'):
-#         subject.greeting()
-#         out, err = capsys.readouterr()
-#         assert out == "No problem!"
+def test_no_returns_message(monkeypatch, capsys):
+    with mock.patch.object(builtins, 'input', lambda _: 'n'):
+        subject.greeting()
+        out, err = capsys.readouterr()
+        assert out == "No problem! Please come back later\n"
 
-# def test_yes_returns_menu(monkeypatch):
-#     with mock.patch.object(builtins, 'input', lambda _: 'y'):
-#         assert subject.greeting() == "Great! Here's our menu: "
+def test_yes_returns_menu(monkeypatch, capsys):
+    with mock.patch.object(builtins, 'input', lambda _: 'y'):
+        subject.greeting()
+        out, err = capsys.readouterr()
+        assert out == "Great! Here's our menu: \n"
 
-# def test_neither_returns_try_again(monkeypatch, capsys):
-#     with mock.patch.object(builtins, 'input', lambda _: 'blah'):
-        # subject.greeting()
-        # out, err = capsys.readouterr()
-        # assert err == ValueError
+@pytest.mark.skip
+def test_neither_returns_try_again(monkeypatch, capsys):
+    with mock.patch.object(builtins, 'input', lambda _: 'blah'):
+        subject.greeting()
+        out, err = capsys.readouterr()
+        assert out == "Input not y or n. Please try again!"
 
+def test_returns_menu(capsys):
+    stub_menu.items_as_list.return_value = ['001 - Regular Cod - £7.00']
+    subject.set_menu(stub_menu)
+    subject.show_menu()
+    captured = capsys.readouterr()
+    assert captured.out == '***************MENU***************\n001 - Regular Cod - £7.00\n'
 
-# def test_returns_menu(capsys):
-#     stub_menu.print_menu.return_value = ['001 - Regular Cod - £7.00', '002 - Small Cod - £5.70', '003 - Scampi - £6.50']
-#     subject.show_menu(stub_menu)
-#     captured = capsys.readouterr()
-#     assert captured.out == '***************MENU***************\n001 - Regular Cod - £7.00\n002 - Small Cod - £5.70\n003 - Scampi - £6.50\n'
-
-# def test_ask_for_choice(capsys):
-#     subject.make_choice()
-#     out, err = capsys.readouterr()
-#     assert out == 'Please enter number of food you\'d like to order:  '
+@pytest.mark.skip
+def test_choose_one_item(monkeypatch, capsys):
+    subject.make_choice()
+    out, err = capsys.readouterr()
+    assert out == 'Please enter number of food you\'d like to order:  '
